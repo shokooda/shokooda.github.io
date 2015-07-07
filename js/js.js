ac638fd7952ee2fb1343af2b9bd6c482afb9a0fd
Sara <saraclay@usc.edu>
2015-07-06 17:24:55 -0700

    Added some jQuery

---
+++ js/js.js
@@ -0,0 +1,8 @@
+$('#about-btn').click(function(){
+    $('div[id^=div]').hide();
+    $('#about').show();
+});
+$('#writing-btn').click(function(){
+    $('div[id^=div]').hide();
+    $('#writing').show();
+});
